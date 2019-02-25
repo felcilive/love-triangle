b2dbeeacaee0a109c9d24b5e4c913773c2d96999
felcilive <47640299+felcilive@users.noreply.github.com>
2019-02-25 21:20:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-            var sum = 0;
-            for (var i = 0; i < preferences.length; i++) {
-                var a = preferences[i];
-                var b = preferences[a - 1];
-                var c = preferences[b - 1];
-                if ((preferences[i] == 0) || (preferences[a - 1] == a) || (preferences[b - 1] == b) || (preferences[c - 1] == c) || (c == a) || (a == b) || (b == c))
-                    continue;
-                if (((preferences[a - 1] == b) || (preferences[a - 1] == c)) && ((preferences[b - 1] == c) || (preferences[b - 1] == a)) && ((preferences[c - 1] == a) || (preferences[c - 1] == b))) {
-                    preferences[i] = 0;
-                    preferences[a - 1] = 0;
-                    preferences[b - 1] = 0;
-                    sum++;
-                }
-            }
+       var sum = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        var a = preferences[i];
+        var b = preferences[a - 1];
+        var c = preferences[b - 1];
+        if ((preferences[i] == 0) || (preferences[a - 1] == a) || (preferences[b - 1] == b) || (preferences[c - 1] == c) || (c == a) || (a == b) || (b == c))
+            continue;
 
-            return sum;
+        if (((preferences[a - 1] == b) && (preferences[b - 1] == c) && (preferences[c - 1] == a)) || ((preferences[a - 1] == c) && (preferences[b - 1] == a) && (preferences[c - 1] == b))) {
+            preferences[i] = 0;
+            preferences[a - 1] = 0;
+            preferences[b - 1] = 0;
+            sum++;
+        }
+
+    }
+
+
+    return sum;
         };
